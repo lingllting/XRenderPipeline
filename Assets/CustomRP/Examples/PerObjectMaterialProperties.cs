598e0dffd3e9f8d9300edc894612dc218334e15e
Liuyang <lingllting@vip.qq.com>
2021-07-04 23:26:30 +0800

    Add alpha blend and alpha clip support to shader

--- Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
+++ Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PerObjectMaterialProperties : MonoBehaviour 
 {
 	static int baseColorId = Shader.PropertyToID("_BaseColor");
+	static int cutoffId = Shader.PropertyToID("_Cutoff");
 	static MaterialPropertyBlock block;
 	
 	[SerializeField]
 	Color baseColor = Color.white;
-
+	[SerializeField, Range(0f, 1f)]
+	float cutoff = 0.5f;
+	
 	private void Awake()
 	{
 		OnValidate();
@@ -22,6 +25,7 @@ public class PerObjectMaterialProperties : MonoBehaviour
 			block = new MaterialPropertyBlock();
 		}
 		block.SetColor(baseColorId, baseColor);
+		block.SetFloat(cutoffId, cutoff);
 		GetComponent<Renderer>().SetPropertyBlock(block);
 	}
 }
