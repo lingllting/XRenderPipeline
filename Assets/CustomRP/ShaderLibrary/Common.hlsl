e54da9804ce2fbe2fddbc7d546c9dcd1d048da96
Liuyang <lingllting@vip.qq.com>
2022-05-23 19:39:46 +0800

    Fix shader issue of redefinition of 'unity_Builtins2Array'

--- Assets/CustomRP/ShaderLibrary/Common.hlsl
+++ Assets/CustomRP/ShaderLibrary/Common.hlsl
@@ -9,9 +9,9 @@
 
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+#include "UnityInput.hlsl"
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/UnityInstancing.hlsl"
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/CommonMaterial.hlsl"
-#include "UnityInput.hlsl"
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
 
 
